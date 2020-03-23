34007efd1673d8a8b61bb8361547f368d3e3fdb9
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-03-23 21:19:34 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,5 +1,18 @@
 ## code your solution here. 
 class Cat 
   attr_accessor :name 
-  def initialize(name)
+  def initialize(Maru)
+    @name = maru 
+  end
+end
+
+def name 
+  @name
+end
+
+def name (value)
+  @name = value 
+end
+ 
+
     
